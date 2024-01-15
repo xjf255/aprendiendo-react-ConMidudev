8ce225c60160fd3768dcf39c2552ea9cf87c7393
xjf255 <juanfher.255@gmail.com>
2024-01-15 12:37:18 -0600

    first part

--- projects/react-prueba-tecnica/src/App.jsx
+++ projects/react-prueba-tecnica/src/App.jsx
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react"
+import './app.css'
 export const App = () => {
+  const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+  const [fact, setFact] = useState()
+  const [imgUrl, setImgUrl] = useState()
+
+  useEffect(() => {
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then(res => res.json())
+      .then(data => {
+        const { fact } = data
+        setFact(fact)
+      })
+  }, [])
+
+  useEffect(() => {
+    if (!fact) return
+    const threefirstword = fact.split(' ', 3).join(' ')
+    const Url = `https://cataas.com/cat/says/${threefirstword}?fontSize=50&fontColor=red`
+    setImgUrl(Url)
+  }, [fact])
+
   return (
     <>
       <h1>hola Mundo</h1>
+      <section>
+        {fact && <p>{fact}</p>}
+        {imgUrl && <img src={imgUrl} alt={`image extracted using the first three words for ${fact}`} />}
+      </section>
     </>
   )
 }
