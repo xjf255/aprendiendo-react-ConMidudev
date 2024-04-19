4bbaaef153f9dbff1b1522cf7e3a89fe57256a28
xjf255 <juanfher.255@gmail.com>
2024-04-18 22:38:41 -0600

    initializated

---
+++ projects/crud_redux/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { ListOfUsers } from "./components/ListOfUsers";
+
+function App() {
+	return (
+		<>
+			<h1>Nuestro projecto con redux</h1>
+			<ListOfUsers />
+		</>
+	);
+}
+
+export default App;
